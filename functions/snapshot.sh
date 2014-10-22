51e3c8c5861b2eeb64b09368ee5d0c8606bf2476
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-10-22 18:51:17 +0400

    new snapshot functions

--- functions/snapshot.sh
+++ functions/snapshot.sh
@@ -3,21 +3,31 @@ set -x
 
 # This file contains the functions to manage VMs images through Qemu-img
 
-QEMUIMG=/usr/bin/qemu-img
-PATH_IMG=/home/kvm/
-
 snapshot_list() {
     imgname=$1
+    echo `qemu-img snapshot -l $imgname|awk 'NR>2{print $2}'`
+}
+
+snapshot_list_id() {
+    imgname=$1
+    echo `qemu-img snapshot -l $imgname|awk 'NR>2{print $1}'`
 }
 
 snapshot_revert () {
-    vmname=$1
-    imgname=$2
-    snapshotname=$3
+    imgname=$1
+    snapshotname=$2
+    qemu-img snapshot -a $snapshotname $imgname
 }
 
 snapshot_create () {
-    vmname=$1
     imgname=$2
+    snapshotname=$2
+    qemu-img snapshot -c $snapshotname $imgname
 }
 
+
+snapshot_delete () {
+    imgname=$2
+    snapshotname=$2
+    qemu-img snapshot -d $snapshotname $imgname
+}
