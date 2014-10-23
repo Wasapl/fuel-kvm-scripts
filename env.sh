3c65e239c47019f634ecdeec78082be317e67aae
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-10-23 17:53:43 +0400

    Adding env.sh

---
+++ env.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set +x
+
+declare -A env
+
+env[lvm]="4.1-compute-lvm
+4.1-controller-lvm
+4.1-compute-lvm-2
+4.1-controller-lvm-2"
+
+env[ceph]="4.1-compute-ceph
+4.1-controller-ceph
+4.1-compute-ceph-2
+4.1-controller-ceph-2"
+
+env[eceph]="4.1-compute-ephem-ceph
+4.1-controller-ephem-ceph
+4.1-compute-ephem-ceph-2
+4.1-controller-ephem-ceph-2"
+
+env[folsom]="compute_nova_volume        
+controller_nova_volume      
+compute_nova_volume_ubuntu      
+controller_nova_volume_ubuntu"
+
+# echo ${env[$1]}
+
+# 1. shutdown all VM (so there is no Envs running on same KVM simultaneously)
+# stop only those VM enlisted in envs, so we do not stop something else
+running=$(get_vms_running)
+for k in ${!env[@]}; do
+    # echo ${env[$k]}
+    for node in ${env[$k]}; do
+        # stop only those VMs that running
+        if [[ $running = *$node* ]]; then
+            echo "virsh stop $node"
+            # virsh stop $node
+        fi
+    done
+done
+
+# 2. revert VMs to snapshots with clear deploy.
+
+
+
+# 3. start VMs. wait till it starts normally.
+
+# 4. do tests (if any)
+
+# 5. create snapshots of VMs and make them available as artifacts in Jenkins.
