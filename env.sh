c71ece5e30b081ca514600d9796fd2bae0148797
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-11-13 13:26:54 +0300

    Using meaningful variable name ENV_NAME insteadof $1

--- env.sh
+++ env.sh
@@ -6,8 +6,10 @@ source functions/env.sh
 source functions/snapshot.sh
 source vms.sh
 
-# Check if $1 is given
-if [ -z $1 ]; then
+ENV_NAME=$1
+
+# Check if $ENV_NAME is given
+if [ -z $ENV_NAME ]; then
     env_names=""
     for K in "${!env[@]}"; do
         if [ -z "$env_names" ]; then env_names=$K;
@@ -19,9 +21,9 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-# Check if $1 contains right Env names.
-if [ ! ${env[$1]+abc} ]; then
-    echo "there is no such Env as '$1'"
+# Check if $ENV_NAME contains right Env names.
+if [ ! ${env[$ENV_NAME]+abc} ]; then
+    echo "there is no such Env as '$ENV_NAME'"
     exit 1
 fi
 
@@ -29,13 +31,13 @@ fi
 stop_all_env $env
 
 # 2. revert VMs to snapshots with clear deploy.
-cleanup_env $1 $env
+cleanup_env $ENV_NAME $env
 
 # 3. start VMs. wait till it starts normally.
-start_env $1 $env
+start_env $ENV_NAME $env
 
 # 4. do tests (if any)
 echo "do some tests (if any)"
 
 # 5. create snapshots of VMs so we could make them available as artifacts in Jenkins.
-make_snapshots_env $1 "trololo`date +%F-%H-%M-%S`" $env
+make_snapshots_env $ENV_NAME "trololo`date +%F-%H-%M-%S`" $env
