0abde07a6e22d6ddab4e07786dca73de9bfc35a4
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-11-14 16:35:46 +0300

    Creating Env arrays for each KVM host

---
+++ osmi-kvm04-vms.sh
@@ -0,0 +1,46 @@
+# This file contains VMs and snapshots names.
+# It could be different from host to host
+
+
+# declaring array with environments
+declare -A env
+env[lvm]="4.1-compute-lvm
+4.1-controller-lvm
+4.1-compute-lvm-2
+4.1-controller-lvm-2"
+env[lvmnovanet]="4.1-compute-lvm-novanet
+4.1-controller-lvm-novanet
+4.1-compute-lvm-novanet-vlan
+4.1-controller-lvm-novanet-vlan"
+env[ceph]="4.1-compute-ceph
+4.1-controller-ceph
+4.1-compute-ceph-2
+4.1-controller-ceph-2"
+env[eceph]="4.1-compute-ceph-ephem
+4.1-controller-ceph-ephem
+4.1-compute-ceph-ephem-2
+4.1-controller-ceph-ephem-2"
+
+
+# declaring array with revert snapshots 
+declare -A snapshots
+#lvm
+snapshots[4.1-compute-lvm]="clean-install"
+snapshots[4.1-controller-lvm]="clean-install"
+snapshots[4.1-compute-lvm-2]="clean-install"
+snapshots[4.1-controller-lvm-2]="clean-install"
+#lvmnovanet
+snapshots[4.1-compute-lvm-novanet]="clean-install"
+snapshots[4.1-controller-lvm-novanet]="clean-install"
+snapshots[4.1-compute-lvm-novanet-vlan]="clean-install"
+snapshots[4.1-controller-lvm-novanet-vlan]="clean-install"
+#ceph
+snapshots[4.1-compute-ceph]="clean-install"
+snapshots[4.1-controller-ceph]="clean-install"
+snapshots[4.1-compute-ceph-2]="clean-install"
+snapshots[4.1-controller-ceph-2]="clean-install"
+#eceph
+snapshots[4.1-compute-ceph-ephem]="clean-install"
+snapshots[4.1-controller-ceph-ephem]="clean-install"
+snapshots[4.1-compute-ceph-ephem-2]="clean-install"
+snapshots[4.1-controller-ceph-ephem-2]="clean-install"
