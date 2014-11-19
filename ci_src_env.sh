c6a673c86eddb4aa06d773c81d5218c3118a3875
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-11-20 02:46:03 +0300

    Add ci jobs scripts

---
+++ ci_src_env.sh
@@ -0,0 +1,24 @@
+source functions/vm.sh
+source functions/env.sh
+source functions/snapshot.sh
+# source arrays which contains VMs and snapshots names.
+arrays=./$(hostname)-vms.sh
+if [ ! -f $arrays ]; then
+  echo "there is no array declarations for host '$(hostname)'"
+  exit 1
+fi
+source ./$(hostname)-vms.sh
+
+# get all env names
+env_names=""
+for K in "${!env[@]}"; do
+    if [ -z "$env_names" ]; then env_names=$K;
+    else env_names="$env_names , $K";
+    fi;
+done
+
+# Check if $SRC_ENV contains right Env names.
+if [ ! ${env[${SRC_ENV}-1]+abc} ]; then
+    echo "there is no such Env as '$SRC_ENV'"
+    exit 1
+fi
---
+++ cloudferry_unittests.sh
@@ -0,0 +1,72 @@
+#http://stackoverflow.com/questions/5506110/is-it-possible-to-install-another-version-of-python-to-virtualenv
+
+path=$PWD
+py_path=$path/.python27
+
+#install python2.7.2
+if [ ! -d $py_path ]; then
+	PY_VER=2.7.2
+	if [ ! -d $path/src ]; then mkdir $path/src; fi
+	if [ ! -d $py_path ]; then mkdir $py_path; fi
+	cd ./src
+	if [ ! -d Python-${PY_VER} ]; then
+		wget http://www.python.org/ftp/python/${PY_VER}/Python-${PY_VER}.tgz
+		tar -zxvf Python-${PY_VER}.tgz
+	fi 
+	cd Python-${PY_VER}
+	if [ -d Python-${PY_VER} ]; then
+		make clean 
+	fi
+	./configure --prefix=$py_path
+	make
+	make install
+fi
+
+#install virtualenv
+VENV_VER=1.11.6
+cd $path/src
+wget http://pypi.python.org/packages/source/v/virtualenv/virtualenv-${VENV_VER}.tar.gz
+tar -zxvf virtualenv-${VENV_VER}.tar.gz
+cd virtualenv-${VENV_VER}/
+$py_path/bin/python setup.py install
+
+#install pip
+cd $path/src
+wget https://bootstrap.pypa.io/get-pip.py
+$py_path/bin/python get-pip.py
+
+
+#create venv
+cd $path
+$py_path/bin/virtualenv venv --python=$py_path/bin/python2.7
+#activate venv
+source venv/bin/activate
+
+#write some requirements.txt
+cd $path
+cat <<ENDOFREQ >requirements.txt
+python-novaclient
+python-cinderclient
+python-glanceclient
+python-keystoneclient
+fabric>=1.8.2
+paramiko>=1.8.0
+oslo.config>=1.2.0
+jinja2
+oslotest
+virtualenv
+sqlalchemy
+python-neutronclient>=2.3.0,<3
+ipaddr
+nose
+ENDOFREQ
+pip install -r requirements.txt
+
+
+#run tests
+cd $path
+venv/bin/python --version
+venv/bin/python venv/bin/nosetests -v
+
+#deactivate venv
+deactivate
