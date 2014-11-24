c5b8671138a8337823187053f03899a8fe8f0b92
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-11-24 15:15:12 +0300

    Improve Operability check

--- ci-src-env.sh
+++ ci-src-env.sh
@@ -26,45 +26,51 @@ fi
 cleanup_env ${SRC_ENV}-1 $env
 start_env ${SRC_ENV}-1 $env
 
+sleep 120
+
 #test Env operability
-ssh_options='-oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no'
-read fuelhost envid <<< "${fuel[${SRC_ENV}-1]}"
-username=root
-password=r00tme
-prompt='root@fuel ~]#'
-#ssh fuel
-#fuel health --check smoke --env $envid
-result=$(
-    expect << ENDOFEXPECT
-    spawn ssh $ssh_options $username@$fuelhost
-    expect "connect to host" exit
-    expect "*?assword:*"
-    send "$password\r"
-    expect "$prompt"
-    send "fuel health --check smoke --env $envid\r"
-    expect "$prompt"
-ENDOFEXPECT
-)
+if [ ! ${fuel[${SRC_ENV}-1]+abc} ]; then
+    echo "There is no fuel node for this Env. Skip test Env operability."
+else
+    ssh_options='-oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no'
+    read fuelhost envid <<< "${fuel[${SRC_ENV}-1]}"
+    username=root
+    password=r00tme
+    prompt='root@fuel ~]#'
+    #ssh fuel
+    #fuel health --check smoke --env $envid
+    result=$(
+        expect << ENDOFEXPECT
+        spawn ssh $ssh_options $username@$fuelhost
+        expect "connect to host" exit
+        expect "*?assword:*"
+        send "$password\r"
+        expect "$prompt"
+        send "fuel health --check smoke --env $envid\r"
+        expect "$prompt"
+    ENDOFEXPECT
+    )
+    echo $result
 
-# When you are launching command in a sub-shell, there are issues with IFS (internal field separator)
-# and parsing output as a set of strings. So, we are saving original IFS, replacing it, iterating over lines,
-# and changing it back to normal
-#
-# http://blog.edwards-research.com/2010/01/quick-bash-trick-looping-through-output-lines/
-OIFS="${IFS}"
-NIFS=$'\n'
-IFS="${NIFS}"
-#check output for 'failure'
-#exit 1 if any failures
-for line in $result; do
-    IFS="${OIFS}"
-    if [[ $line == *failure* ]]; then
-    	IFS="${NIFS}"
-    	echo "There were failures in health check. Exiting."
-        exit 1;
-    fi    
+    # When you are launching command in a sub-shell, there are issues with IFS (internal field separator)
+    # and parsing output as a set of strings. So, we are saving original IFS, replacing it, iterating over lines,
+    # and changing it back to normal
+    #
+    # http://blog.edwards-research.com/2010/01/quick-bash-trick-looping-through-output-lines/
+    OIFS="${IFS}"
+    NIFS=$'\n'
     IFS="${NIFS}"
-done
-
+    #check output for 'failure'
+    #exit 1 if any failures
+    for line in $result; do
+        IFS="${OIFS}"
+        if [[ $line == *failure* ]]; then
+        	IFS="${NIFS}"
+        	echo "There were failures in health check. Exiting."
+            exit 1;
+        fi
+        IFS="${NIFS}"
+    done
+fi
 
 #create Objects
