e2244dc1a85c5116a1859424f7a94d460a48fd07
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-12-12 04:33:28 +0300

    create-objects.sh continue coding

--- create-objects.sh
+++ create-objects.sh
@@ -2,7 +2,10 @@
 
 #ssh controller
 
-# 1. create tenants [admin, testtenant]
+# create flavor with ephem drive, mem 1GB, rootdisk 1G
+nova flavor-create v1.little auto 1024 0 1 --ephemeral 10
+
+# create tenants [admin, testtenant]
 PREFIX=DEMO
 if keystone tenant-create --name ${PREFIX}prj --description 'Test project for migration'; then
     tid=$( keystone tenant-get ${PREFIX}prj |awk 'BEGIN{FS="|"}$2~/ id /{gsub(/^[ \t]+|[ \t]+$/, "", $3);print $3;}' )
@@ -16,16 +19,16 @@ if keystone tenant-create --name ${PREFIX}prj --description 'Test project for mi
     OS_USERNAME=${PREFIX}u1
     OS_PASSWORD=PASSWORD
 
-    # 4. create routers: [r1, r2]
+    # create routers: [r1, r2]
     for r in R1 R2; do
         quantum router-create ${PREFIX}$r
     done
 
-    # 2. create networks [net1, net2]
+    # create networks [net1, net2]
     i=1
     for net in net1 net2; do
         quantum net-create --tenant-id $tid ${PREFIX}$net 
-        # 3. create subnetworks: net1: [subnet1, subnet2] , net2: [subnet3, subnet4]
+        # create subnetworks: net1: [subnet1, subnet2] , net2: [subnet3, subnet4]
         for sub in sub1 sub2; do
             snetid=$( quantum subnet-create --tenant-id $tid demo-net 10.5.$1.0/24 --gateway 10.5.$1.1 --name ${PREFIX}${net}$sub \
                 | awk 'BEGIN{FS="|"}$2~/ id /{gsub(/^[ \t]+|[ \t]+$/, "", $3);print $3;}' )
@@ -36,29 +39,29 @@ if keystone tenant-create --name ${PREFIX}prj --description 'Test project for mi
     done
 
 
-    # 5. create Security groups [SG1, SG2]
+    # create Security groups [SG1, SG2]
     for sg in SG1 SG2; do
         nova secgroup-create ${PREFIX}$sg "${PREFIX} $sg"
-        # 6. create rules in each SG: TCP in and out, UDP in and out
+        # create rules in each SG: TCP in and out, UDP in and out
         nova secgroup-add-rule ${PREFIX}$sg tcp 443 443 0.0.0.0/0
         nova secgroup-add-rule ${PREFIX}$sg udp 443 443 0.0.0.0/0
     done
 
-    # 10. create 2 keypair
+    # create 2 keypair
     for kp in KP1 KP2; do
         nova keypair-add ${PREFIX}$kp
     done
 
-
+    # create instance booted from image with flavor from step 11. shut it off
+    nova boot --flavor v1.little --image TestVM --key-name ${PREFIX}KP1 ${PREFIX}-VM
+    # create instance booted from volume with flavor from step 11. shut it off
+    # create 2 floating IP
+    #nova add-floating-ip <server> <address>
 fi
 
-# 8. create 2 images
+# create 2 images
 glance image-create --name "Ubuntu 14.04 server-amd64" --disk-format iso --copy-from http://releases.ubuntu.com/14.04/ubuntu-14.04.1-server-amd64.iso --is-public True
-# 9. create 2 volumes
+# create 2 volumes
 
-# 11. create flavor with ephem drive, mem 1GB, rootdisk 1G
-nova flavor-create v1.little auto 1024 0 1 --ephemeral 10 
-# 12. create instance booted from image with flavor from step 11. shut it off
-# 13. create instance booted from volume with flavor from step 11. shut it off
-# 7. create 2 floating IP
-#nova add-floating-ip <server> <address>
+
+
