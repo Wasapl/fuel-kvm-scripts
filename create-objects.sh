cccc74e860fa7c7584e8876ed96a60d766ee052b
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-12-19 11:53:51 +0300

    Make quantum calls parametrized

--- create-objects.sh
+++ create-objects.sh
@@ -2,11 +2,14 @@
 
 #ssh controller
 
+PREFIX=DEMO
+NEUTRON=quantum
+
 # create flavor with ephem drive, mem 1GB, rootdisk 1G
 nova flavor-create v1.little auto 1024 0 1 --ephemeral 10
 
 # create tenants [admin, testtenant]
-PREFIX=DEMO
+
 if keystone tenant-create --name ${PREFIX}prj --description 'Test project for migration'; then
     tid=$( keystone tenant-get ${PREFIX}prj |awk 'BEGIN{FS="|"}$2~/ id /{gsub(/^[ \t]+|[ \t]+$/, "", $3);print $3;}' )
 
@@ -21,19 +24,19 @@ if keystone tenant-create --name ${PREFIX}prj --description 'Test project for mi
 
     # create routers: [r1, r2]
     for r in R1 R2; do
-        quantum router-create ${PREFIX}$r
+        $NEUTRON router-create ${PREFIX}$r
     done
 
     # create networks [net1, net2]
     i=1
     for net in net1 net2; do
-        quantum net-create --tenant-id $tid ${PREFIX}$net 
+        $NEUTRON net-create --tenant-id $tid ${PREFIX}$net 
         # create subnetworks: net1: [subnet1, subnet2] , net2: [subnet3, subnet4]
         for sub in sub1 sub2; do
-            snetid=$( quantum subnet-create --tenant-id $tid demo-net 10.5.$1.0/24 --gateway 10.5.$1.1 --name ${PREFIX}${net}$sub \
+            snetid=$( $NEUTRON subnet-create --tenant-id $tid demo-net 10.5.$1.0/24 --gateway 10.5.$1.1 --name ${PREFIX}${net}$sub \
                 | awk 'BEGIN{FS="|"}$2~/ id /{gsub(/^[ \t]+|[ \t]+$/, "", $3);print $3;}' )
-            quantum router-interface-add ${PREFIX}R1 $snetid
-            quantum router-interface-add ${PREFIX}R2 $snetid
+            $NEUTRON router-interface-add ${PREFIX}R1 $snetid
+            $NEUTRON router-interface-add ${PREFIX}R2 $snetid
         done
         let 'i = i + 1'
     done
