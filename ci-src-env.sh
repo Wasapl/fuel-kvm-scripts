fb3e33ba2d572af5145a864acb5c19bf8e3f8c64
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2014-12-23 12:02:54 +0300

    Intergating ostf tests with CI-src-env.sh

--- ci-src-env.sh
+++ ci-src-env.sh
@@ -32,47 +32,11 @@ start_env $ENV_NAME $env
 echo "sleep for 4 minutes in order to let Env spawn up before testing it"
 sleep 240
 
+
 #test Env operability
 if [ ! ${fuel[$ENV_NAME]+abc} ]; then
     echo "There is no fuel node for this Env. Skip test Env operability."
 else
-    ssh_options='-oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no'
     read fuelhost envid <<< "${fuel[$ENV_NAME]}"
-    username=root
-    password=r00tme
-    prompt='root@fuel ~]#'
-    #ssh fuel
-    #fuel health --check smoke --env $envid
-    result=$(
-        expect << ENDOFEXPECT
-        spawn ssh $ssh_options $username@$fuelhost
-        expect "*?assword:*"
-        send "$password\r"
-        expect "$prompt"
-        set timeout 360
-        send "fuel health --check smoke --env $envid\r"
-        expect "$prompt"
-ENDOFEXPECT
-    )
-    echo $result
-
-    # When you are launching command in a sub-shell, there are issues with IFS (internal field separator)
-    # and parsing output as a set of strings. So, we are saving original IFS, replacing it, iterating over lines,
-    # and changing it back to normal
-    #
-    # http://blog.edwards-research.com/2010/01/quick-bash-trick-looping-through-output-lines/
-    OIFS="${IFS}"
-    NIFS=$'\n'
-    IFS="${NIFS}"
-    #check output for 'failure'
-    #exit 1 if any failures
-    for line in $result; do
-        IFS="${OIFS}"
-        if [[ $line == *failure* ]]; then
-            IFS="${NIFS}"
-            echo "There were failures in health check. Exiting."
-            exit 1;
-        fi
-        IFS="${NIFS}"
-    done
+    source poke-ostf.sh $fuelhost $envid
 fi
